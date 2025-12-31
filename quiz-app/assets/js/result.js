3c8900bd2d1942d3889be2ae0c18f1fc65e9597e
ThanhTin926 <thanhtinvonguyen926@gmail.com>
2025-12-31 21:56:24 +0700

    Delete quiz-app directory

--- quiz-app/assets/js/result.js
+++
@@ -1,5 +0,0 @@
-const result = JSON.parse(localStorage.getItem("last_result"));
-document.getElementById("result").innerHTML = `
-  <p>✔ Số câu đúng: ${result.correct}/${result.total}</p>
-  <p>🎯 Điểm: ${result.score}%</p>
-`;
--- quiz-app/assets/js/storage.js
+++
@@ -1,5 +0,0 @@
-function saveHistory(result) {
-  const history = JSON.parse(localStorage.getItem("quiz_history") || "[]");
-  history.push(result);
-  localStorage.setItem("quiz_history", JSON.stringify(history));
-}
